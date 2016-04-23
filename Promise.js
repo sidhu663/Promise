bfef6bca4ad78765d12bcce0761b921d4386d65d
sidhu663 <sidhu663@gmail.com>
2016-04-22 17:38:45 -0700

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -1,53 +1,53 @@
 function Promise(executor) {
-	var successCallbacks = [];
+  var successCallbacks = [];
   var failureCallbacks = [];
-  var state = 'pending'; 
-  
+  var state = 'pending';
+
   var resolve = function(resolutionValue) {
-		if (state != 'pending') { return; }
+    if (state != 'pending') { return; }
     successCallbacks.forEach(f => f(resolutionValue));
     state = 'resolved';
     result = resolutionValue;
   }
-  
+
   var reject = function(rejectionValue) {
-  	if (state != 'pending') { return; }
+    if (state != 'pending') { return; }
     failureCallbacks.forEach(f => f(rejectionValue));
     state = 'rejected';
     result = rejectionValue;
   }
-  
+
   this.then = function(s, f) {
-  	s = s || (x => x);
+    s = s || (x => x);
     f = f || (x => x);
-  	return new Promise(function(resolve, reject) {
-    	var successCallback = function(resolution) {
-      	var callbackResult = s(resolution);
+    return new Promise(function(resolve, reject) {
+      var successCallback = function(resolution) {
+        var callbackResult = s(resolution);
         if (typeof callbackResult == 'object' && 'then' in callbackResult) {
-        	callbackResult.then(resolve, reject);
+          callbackResult.then(resolve, reject);
         } else {
-        	resolve(callbackResult);
+          resolve(callbackResult);
         }
       };
-      
+
       var failureCallback = function(rejection) {
-      	var callbackResult = s(resolution);
+        var callbackResult = s(resolution);
         if (typeof callbackResult == 'object' && 'then' in callbackResult) {
-        	callbackResult.then(resolve, reject);
+          callbackResult.then(resolve, reject);
         } else {
-        	reject(callbackResult);
+          reject(callbackResult);
         }
       };
-      
-    	if (state != 'pending') {
-      	state == 'resolved' ? successCallback(result) : failureCallback(result);
+
+      if (state != 'pending') {
+        state == 'resolved' ? successCallback(result) : failureCallback(result);
       } else {
-      	successCallbacks.push(successCallback);
+        successCallbacks.push(successCallback);
         failureCallbacks.push(failureCallback);
       }
     });
   };
-  
+
   executor(resolve, reject);
 }
 
